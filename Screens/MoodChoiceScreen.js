aacae0e22a90632f2103d5fe19fdaf8b9e9d9e2f
fourbet <ophelie.fourbet@st.ovgu.de>
2021-06-15 14:30:32 +0200

    feat(): mood user db

--- Screens/MoodChoiceScreen.js
+++ Screens/MoodChoiceScreen.js
@@ -1,16 +1,45 @@
 import { NavigationContainer } from '@react-navigation/native';
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { StyleSheet, Text, View, Pressable, Image } from 'react-native'
-
+import { db, firebase } from "../firebase";
 
 
 const MoodChoiceScreen = ({ navigation }) => {
 
 
     const [question, setQuestion] = useState('Hello, how are you feeling today?');
+    const [idUser, setIdUser] = useState("");
+    const [mood, setMood] = useState("");
+    const date = ""
 
-    const onPressHandler = () => {
-        navigation.navigate('ActivityChoice')
+    useEffect(() => {
+        if (idUser != "") {
+            add_user_mood();
+
+        }
+    });
+
+
+
+
+    // Add a new document with a generated id in the collection user_mood
+    const add_user_mood = () => {
+        const date = new Date().getDate();
+        const month = new Date().getMonth() + 1;
+        const year = new Date().getFullYear();
+        db.collection("user_mood").add({
+            id_user: idUser,
+            name_mood: mood,
+            date: (date + '-' + month + '-' + year),
+        });
+        navigation.navigate('ActivityChoice');
+    }
+
+    // Set activity Id and user Id
+    function set_user_mood() {
+
+        if (firebase.auth().currentUser !== null)
+            setIdUser(firebase.auth().currentUser.uid);
 
     }
 
@@ -28,23 +57,35 @@ const MoodChoiceScreen = ({ navigation }) => {
 
             <View style={styles.row}>
                 <Pressable
-                    onPress={onPressHandler}
+                    onPress={() => {
+                        setMood('very happy');
+                        set_user_mood();
+                    }}
                 >
-                    <Image style={styles.image}  source={require('../assets/smiley-very-happy.png')} />
+                    <Image style={styles.image} source={require('../assets/smiley-very-happy.png')} />
                 </Pressable>
                 <Pressable
-                    onPress={onPressHandler}
+                    onPress={() => {
+                        setMood('happy');
+                        set_user_mood();
+                    }}
                 >
-                    <Image style={styles.image}  source={require('../assets/smiley-blushed.png')} />
+                    <Image style={styles.image} source={require('../assets/smiley-blushed.png')} />
                 </Pressable>
                 <Pressable
-                    onPress={onPressHandler}
+                 onPress={() => {
+                    setMood('sad');
+                    set_user_mood();
+                }}
                 >
                     <Image style={styles.image} source={require('../assets/smiley-sad.png')} />
                 </Pressable>
 
                 <Pressable
-                    onPress={onPressHandler}
+                     onPress={() => {
+                        setMood('deplored');
+                        set_user_mood();
+                    }}
                 >
                     <Image style={styles.image} source={require('../assets/smiley-deplored.png')} />
                 </Pressable>
