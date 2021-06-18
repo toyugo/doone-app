b412cf2e560bd90dff160a73741ec75a3aa5f7d5
o <o@MacBookPro-B8E85644050A.home>
2021-06-18 11:49:00 +0200

    ResultScreen

--- Screens/ResultsScreen.js
+++ Screens/ResultsScreen.js
@@ -1,64 +1,94 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { Modal, TextInput, Button, FlatList, Linking, RefreshControl, ScrollView, SectionList, StyleSheet, Text, View, Touchable, TouchableOpacity, TouchableHighlight, TouchableWithoutFeedback, Pressable, Alert, ToastAndroid } from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack';
-
+import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  Modal,
+  TextInput,
+  Button,
+  FlatList,
+  Linking,
+  RefreshControl,
+  ScrollView,
+  SectionList,
+  StyleSheet,
+  Text,
+  View,
+  Touchable,
+  TouchableOpacity,
+  TouchableHighlight,
+  TouchableWithoutFeedback,
+  Pressable,
+  Alert,
+  ToastAndroid,
+} from "react-native";
+import { db, firebase } from "../firebase";
+import { createStackNavigator } from "@react-navigation/stack";
 
 export default function ResultsScreen({ navigation }) {
+  const onPressHandler = () => {
+    // navigation.navigate('Screen_A')
+  };
+  const [counterType, setCounterType] = useState([]);
+  const [listIdActivities, setListIdActivities] = useState([]);
+  const [idUser, setIdUser] = useState("");
 
-    const onPressHandler = () => {
-        // navigation.navigate('Screen_A')
-    }
+  function get_activities_id() {
+    const tb = [];
+    if (firebase.auth().currentUser !== null)
+      setIdUser(firebase.auth().currentUser.uid);
+    else return;
+    db.collection("user_activity")
+      .where("id_user", "==", idUser)
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          tb.push(doc.data().id_activity);
+        });
+      });
+    setListIdActivities(tb);
+  }
 
-
-    return (
-        <View style={styles.body}>
-            <Text style={styles.text}>
-                Results Screen
-        </Text>
-
-            {/*         <Text style={styles.text}>
-          Screen B
-        </Text>
-  
-        <Pressable
-        onPress={onPressHandler}
-        style={({pressed})=> ({backgroundColor: pressed ? '#ddd' : '#0f0'})}
-        
-        >
-          <Text style={styles.text}>
-  
-            Go back to Screen A
-          </Text>
-  
-        </Pressable> */}
-
-        </View>
-
-
-    )
+  const set_CountPersonal = () => {
+    get_activities_id();
+    let tbNumber = [];
+    let tb = [];
+    const counter = { social: 0, cultural: 0, personal: 0 };
+    db.collection("activity")
+      .get()
+      .then(function (querySnapshot) {
+        console.log(tb);
+        querySnapshot.forEach(function (doc) {
+          for (let i = 0; i < listIdActivities.length; i++) {
+            if (doc.id == listIdActivities[i]) {
+              counter[doc.data().type] = counter[doc.data().type] + 1;
+            }
+          }
+        });
+        console.log("counter value =>", counter);
+        setCounterType(counter);
+      });
+  };
+  return (
+    <View style={styles.body}>
+      <Text style={styles.text}>Results Screen</Text>
+      <Text style={styles.text}>{counterType["social"]}</Text>
+      <Text style={styles.text}>{counterType["cultural"]}</Text>
+      <Text style={styles.text}>{counterType["personal"]}</Text>
+      <Button title="test" onPress={set_CountPersonal}></Button>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
+  body: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 
-    body: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-
-
-
-    },
-
-
-
-    text: {
-        fontSize: 40,
-        fontWeight: 'bold',
-        margin: 10,
-
-
-    }
-
-})
+  text: {
+    fontSize: 40,
+    fontWeight: "bold",
+    margin: 10,
+  },
+});
