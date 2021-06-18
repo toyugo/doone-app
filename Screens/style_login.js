7339380745dc391b929810716f4437edad9b46ae
o <o@MacBookPro-B8E85644050A.home>
2021-06-18 12:09:46 +0200

    style login

---
+++ Screens/style_login.js
@@ -0,0 +1,58 @@
+import { StyleSheet } from "react-native";
+
+const styleLogin = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "#FDECDC",
+  },
+  title: {},
+  logo: {
+    flex: 1,
+    height: 200,
+    width: 200,
+    alignSelf: "center",
+    marginTop: 100,
+  },
+  input: {
+    height: 48,
+    borderRadius: 5,
+    overflow: "hidden",
+    backgroundColor: "white",
+    marginTop: 0,
+    marginBottom: 10,
+    marginLeft: 30,
+    marginRight: 30,
+    paddingLeft: 16,
+  },
+  button: {
+    backgroundColor: "#FFB680",
+    marginLeft: 30,
+    marginRight: 30,
+    marginTop: 20,
+    height: 48,
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonTitle: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  footerView: {
+    flex: 1,
+    alignItems: "center",
+    marginTop: 20,
+  },
+  footerText: {
+    fontSize: 16,
+    color: "#2e2e2d",
+  },
+  footerLink: {
+    color: "#FFB680",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+});
+export default styleLogin;
